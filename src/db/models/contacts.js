92b279f154f5fedca612cbb47393322f04011051
Denys Kolisnychenko <79978293+denisolid@users.noreply.github.com>
2024-10-23 18:11:20 +0000

    Initial commit

---
+++ src/db/models/contacts.js
@@ -0,0 +1,35 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+    userId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contact', contactsSchema);
